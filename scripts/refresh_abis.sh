395c0ce3209918e400cca7fb66c9fb5cbc41c1a9
rleifke <r.leifke@gmail.com>
2021-09-05 17:44:39 -0400

    added abis scrippt

---
+++ scripts/refresh_abis.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)/..
+mkdir -p ./src/abis/dahlia_contracts
+cp -R ../dahlia_contracts/build/contracts/* ./src/abis/dahlia_contracts
+mkdir -p ./src/abis/fountain_of_youth
+cp -R ../fountain_of_youth/build/abi/* ./src/abis/fountain_of_youth
